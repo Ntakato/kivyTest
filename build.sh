f4c236d7b7be71e48cfef9c75916c1b0b71bd0dc
TakatoNakamura <pptaka10gg@gmail.com>
2024-10-17 17:44:53 +0900

    measure time

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+start_time=`date +%s`
+
 # 一応ビルドファイルなどを消す
 rm -rf ./build
 rm -rf ./dist
@@ -14,3 +16,8 @@ poetry install --without dev
 # pyinstallerはremoveしてあるので、pyinstallerを使いたかったら、addする
 # poetry run pyinstaller ./src/main.py --clean --onefile 
 poetry run nuitka --standalone --onefile ./src/main.py 
+
+end_time=`date +%s`
+time=$((end_time - start_time))
+
+echo "Build success, Time taken to build: ${time}s"
