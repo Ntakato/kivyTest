4f5f0e703cf341573c2c8afda08130ccb70f09c1
TakatoNakamura <pptaka10gg@gmail.com>
2024-11-19 23:18:35 +0900

    modal

--- src/multiprocess_sample.py
+++ src/multiprocess_sample.py
@@ -5,6 +5,8 @@ import time
 from kivy.app import App
 from kivy.clock import Clock
 from kivy.lang.builder import Builder
+from kivy.uix.label import Label
+from kivy.uix.modalview import ModalView
 
 
 def factorial(n):
@@ -18,15 +20,29 @@ def factorial(n):
 
 def background_task(queue, n):
     print(f"processID: {os.getpid()}; calculate {n}!")
-    time.sleep(1)
+    time.sleep(0.8)
     result1 = factorial(n)
     queue.put(f"{n}!: {result1}\n{n + 10}!: ")
     print(f"processID: {os.getpid()}; result = {result1}")
     print(f"processID: {os.getpid()}; calculate {n + 10}!")
-    time.sleep(1)
+    time.sleep(0.8)
     result2 = factorial(n + 10)
     queue.put(f"{n}!: {result1}\n{n + 10}!: {result2}")
     print(f"processID: {os.getpid()}; result = {result2}")
+
+
+def image_processing(x):
+    print('start image processing')
+    time.sleep(2)
+    print('end image processing')
+    return x
+
+
+def processing():
+    print("start calculate processing")
+    time.sleep(2)
+    print("end calculate processing")
+    return "success"
 
 
 kv = '''
@@ -43,16 +59,6 @@ BoxLayout:
             group: 'a'
             text: '10, 20'
             on_press: app.button_pressed(10)
-        ToggleButton:
-            id: btn3
-            group: 'a'
-            text: '20, 30'
-            on_press: app.button_pressed(20)
-        ToggleButton:
-            id: btn4
-            group: 'a'
-            text: '30, 40'
-            on_press: app.button_pressed(30)
     Label:
         id: label
         status: 'Reading'
@@ -62,22 +68,41 @@ BoxLayout:
 
 class SampleApp(App):
 
+    images = []
+
     def build(self):
         print(f"processID: {os.getpid()}; this is main process")
         self.queue = multiprocessing.Queue()
-        Clock.schedule_interval(self.check_queue, 1)
+        Clock.schedule_interval(self.check_queue, 0.5)
         return Builder.load_string(kv)
 
     def button_pressed(self, n):
+        self.processing_percentage = 0
+        self.process_text = f"Processing\n{self.processing_percentage}%"
+
+        # 操作可能な部分がないフルスクリーンのモーダルを出すことでユーザー操作を制限します
+        self.modal = ModalView(size_hint=(.8, .8), auto_dismiss=False)
+        self.modal_label = Label(text=self.process_text)
+        self.modal.add_widget(self.modal_label)
+        self.modal.open()
+
         self.process = multiprocessing.Process(target=background_task, args=(self.queue, n))
         self.process.start()
-        self.root.ids.label.text = "Processing..."
         # print(self.process.is_alive())
 
     def check_queue(self, dt):
         if not self.queue.empty():
             result = self.queue.get()
-            self.root.ids.label.text = f"{result}"
+            self.processing_percentage += 50
+            self.process_text = f"Processing\n{self.processing_percentage}%"
+            self.modal_label.text = self.process_text
+            self.images.append(image_processing(result))
+            print(self.images)
+            if (len(self.images) == 2):
+                final_result = processing()
+                self.root.ids.label.text = f"{result}\n{final_result}"
+                self.images = []
+                self.modal.dismiss()
             # print(self.process.is_alive())
 
 
